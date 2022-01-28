f5d36f035b2401db43292b59ff3cae181a8725c5
Kolinas <45821242+Kolinas@users.noreply.github.com>
2022-01-28 15:06:36 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 const mainImage = document.querySelector('.img_gal');
 const minImage = document.querySelector('.gallery_min');
 
-const images =['min_img1.png', `min_img2.png`, `min_img3.png`];
+const images =['min_img1.jpg', `min_img2.jpg`, `min_img3.jpg`];
 
 
 for (const image of images) {
